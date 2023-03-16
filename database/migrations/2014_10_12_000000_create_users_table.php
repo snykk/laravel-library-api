28ab73b8c8ecf6a871e20d6c18090b75042d1f8d
Najib Fikri <najibfikri13@gmail.com>
2023-03-16 13:26:25 +0700

    add: 'rentals' column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('reviews')->default(0);
+            $table->integer('rentals')->default(0);
             $table->rememberToken();
             $table->timestamps();
 
